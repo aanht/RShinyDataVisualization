49067dd6ad00cae2bc6588c302ebb82f991f986a
Anh Tran <anht@berkeleyedu>
2024-04-15 01:08:57 -0700

    add button input and dropbox input to begin analysis of tobacco data

--- ui.R
+++ ui.R
@@ -8,9 +8,9 @@
 #
 
 library(shiny)
-install.packages("babynames")
 library(babynames)
-library(tidyverse)
+library(sf)
+library(rnaturalearth)
 
 # Define UI for application that draws a histogram
 fluidPage(
@@ -20,23 +20,42 @@ fluidPage(
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("yearRange", "Choose a range of years:", min = 1880, 
-                        max = 2017, value = c(1880, 2017)),
+      sidebarPanel(
+        sliderInput("yearRange", "Choose a range of years:", min = 1880, 
+                    max = 2017, value = c(1880, 2017)),
+
+        selectizeInput(
+          "selectName", "Choose 4 names:", 
+          choices = unique(babynames$name[startsWith(babynames$name, "A")][1:10]), 
+          options = list(maxItems = 4)), 
+        
+        selectizeInput(
+          "selectSex", "Male or Female:", 
+          choices = unique(babynames$sex))
+          
+      ),
+      #Show a plot of the generated distribution
+      mainPanel(
+          plotOutput("linePlot")
+      )
+    ),
     
-            selectizeInput(
-              "selectName", "Choose 4 names:", 
-              choices = unique(babynames$name[startsWith(babynames$name, "A")][1:10]), 
-              options = list(maxItems = 4)), 
-            
-            selectizeInput(
-              "selectSex", "Male or Female:", 
-              choices = unique(babynames$sex))
-            
-        ),
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("linePlot")
-        )
+    #Youth Tobacco Map Plot
+    titlePanel("Tobacco Use Between High School & Middle School"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        radioButtons("buttonsTypeUse",  "Choose the type(s) of tobacco use:",
+                     choices = list("Cigarettes" = 1, "Smokeless Tobacco" = 2, "Cessation (want to quit or quit attempted)" = 3), 
+                     selected = 1),
+        selectizeInput(
+          "selectHabit", "How frequent:", 
+          choices = list("One or two times" = 1, "20-30 days" = 2, "30+ days" = 3),)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+        plotOutput("barPlot")
+      )
     )
 )
