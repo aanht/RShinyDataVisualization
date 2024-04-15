7ba52a66108a84e2148e9de2145e3b49d383e5cb
Anh Tran <anht@berkeleyedu>
2024-04-14 21:38:28 -0700

    change theme of line plot to fit data-ink ratio

--- server.R
+++ server.R
@@ -9,16 +9,24 @@
 
 library(babynames)
 
-
 function(input, output, session) {
-  #Getting inputs, filtering data, graph line plot
+  #Babynames over the years
   output$linePlot <- renderPlot({
+    #Getting inputs
     selected1stYear <- input$selectFirstYear
     selected2ndYear <- input$selectSecondYear
     selectedNames <- input$selectName
-    filtered <- filter(babynames, name %in% selectedNames, year >= selected1stYear & year <= selected2ndYear)
-    ggplot(filtered, aes(year, prop, color = name)) + geom_line()
-    
- 
+    selectedSex <- input$selectSex
+
+    #Filtering data & graph line plot
+    filtered <- filter(babynames, name %in% selectedNames, year >= selected1stYear & year <= selected2ndYear, sex == selectedSex)
+    ggplot(filtered, aes(year, prop, color = name)) + 
+      geom_line() + 
+      scale_color_manual(values = c('goldenrod1', 'palegreen4', 'steelblue', 'indianred2')) + 
+      theme_bw() + 
+      theme(panel.grid.major = element_line(color = "grey", linetype = "dotted"), 
+            panel.grid.minor = element_blank(), 
+            axis.line.x = element_line(color="black"), 
+            axis.line.y = element_line(color="black"))
   })
 }
