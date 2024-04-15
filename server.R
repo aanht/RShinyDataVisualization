61597d8c42870a76e4a41497c5ba7cd1b7866b46
Anh Tran <anht@berkeleyedu>
2024-04-14 20:18:45 -0700

    filtered data given the inputs and make line plot

--- server.R
+++ server.R
@@ -9,23 +9,16 @@
 
 library(babynames)
 
-#Getting inputs and filtering data
+
 function(input, output, session) {
-#Render line plot & graph line plot
+  #Getting inputs, filtering data, graph line plot
   output$linePlot <- renderPlot({
-    #filteredData <- getInputs()
     selected1stYear <- input$selectFirstYear
     selected2ndYear <- input$selectSecondYear
     selectedNames <- input$selectName
     filtered <- filter(babynames, name %in% selectedNames, year >= selected1stYear & year <= selected2ndYear)
-    #ggplot(filtered, aes(year, prop, color = name)) + geom_line() + geom_point()
+    ggplot(filtered, aes(year, prop, color = name)) + geom_line()
     
-    if (nrow(filtered) == 0) {
-      # Print a message if the filtered dataset is empty
-      cat("Filtered dataset is empty.")
-    } else {
-      # Plot line graph if the filtered dataset is not empty
-      ggplot(filtered, aes(year, prop, color = name)) + geom_line() + geom_point()
-    }
+ 
   })
 }
